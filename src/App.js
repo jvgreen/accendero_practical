ed7840a18a45532a8c4eea39fc01b2f8f67d1ec7
Jonah Green <gre11017@byui.edu>
2020-06-10 13:01:16 -0600

    Display new Thickness and add button to calculate
    Added a new Thickness to each valve and a button to calculate the new valve thicknesses.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import ValveList from './components/ValveList';
 import Header from './components/Header';
 import SubHeader from './components/SubHeader';
 import SpectList from './components/SpecList';
+
+/*
+   To learn more about shimming a motorcyle: https://www.youtube.com/watch?v=1lrH1U4FnRY
+   I was unable to get the valves to render dynamically
+   the number of valves box at the moment is usless but I was hoping
+   to get it so number of valves create would reflect the number the user needs
+*/
 class  App extends Component {
   state = {
     // With more time I would make the valves be created
@@ -12,45 +19,41 @@ class  App extends Component {
       {
         id: "Exhaust 1",
         currentGap: 0,
-        ShimThickness: 0
+        ShimThickness: 0,
+        newThickness:0
       },
       {
         id: "Exhaust 2",
         currentGap: 0,
-        ShimThickness: 0
+        ShimThickness: 0,
+        newThickness: 0
       },
       {
         id: "Exhaust 3",
         currentGap: 0,
-        ShimThickness: 0
+        ShimThickness: 0,
+        newThickness: 0
       },
-      {
-        id: "Exhaust 4",
-        currentGap: 0,
-        ShimThickness: 0
-      }
     ],
     inValves: [
       {
         id: "Intake 1",
         currentGap: 0,
-        ShimThickness: 0
+        ShimThickness: 0,
+        newThickness: 0
       },
       {
         id: "Intake 2",
         currentGap: 0,
-        ShimThickness: 0
+        ShimThickness: 0,
+        newThickness: 0
       },
       {
         id: "Intake 3",
         currentGap: 0,
-        ShimThickness: 0
+        ShimThickness: 0,
+        newThickness: 0
       },
-      {
-        id: "Intake 4",
-        currentGap: 0,
-        ShimThickness: 0
-      }
     ],
     specs: [
       {
@@ -90,7 +93,7 @@ class  App extends Component {
   gapChanged = (e) => {
     if(e.target.name.includes("Ex")){
       this.setState({exValves: this.state.exValves.map(exValves =>{
-        if(exValves.id == e.target.name) {
+        if(exValves.id === e.target.name) {
           exValves.currentGap = e.target.value
         }
         return exValves;
@@ -99,7 +102,7 @@ class  App extends Component {
     if(e.target.name.includes("In"))
     {
       this.setState({inValves: this.state.inValves.map(inValves =>{
-        if(inValves.id == e.target.name) {
+        if(inValves.id === e.target.name) {
           inValves.currentGap = e.target.value
         }
         return inValves;
@@ -111,7 +114,7 @@ class  App extends Component {
   thicknessChanged = (e) => {
     if(e.target.name.includes("Ex")){
       this.setState({exValves: this.state.exValves.map(exValves =>{
-        if(exValves.id == e.target.name) {
+        if(exValves.id === e.target.name) {
           exValves.ShimThickness = e.target.value
         }
         return exValves;
@@ -120,12 +123,22 @@ class  App extends Component {
     if(e.target.name.includes("In"))
     {
       this.setState({inValves: this.state.inValves.map(inValves =>{
-        if(inValves.id == e.target.name) {
+        if(inValves.id === e.target.name) {
           inValves.ShimThickness = e.target.value
         }
         return inValves;
       })})
     }
+  }
+
+  // Calculates the new shim thickness
+  //newShimThickness = (currentGap - tolorance) + ShimThickness;
+  calculate = () => {
+    // exhaust
+
+
+    // intake
+   
   }
 
   render() {
@@ -154,7 +167,7 @@ class  App extends Component {
             thicknessChanged={this.thicknessChanged}
             gapChanged={this.gapChanged}  
           /> <br/>
-          <button>Calculate New Shims</button>
+          <button style={{width: '300px', height: '75px'}} onClick={this.calculate}>Calculate New Shims</button>
       </div>
     );
   }
--- src/components/Valve.js
+++ src/components/Valve.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 
 export class Valve extends Component {
    render() {
-      const {id} = this.props.valve;
+      const {id, newThickness} = this.props.valve;
       return (
          <div style={inputStyle}>
             <label>{id}</label> <br/>
             <input type="text" placeholder="Current Gap" name={id} onChange={this.props.gapChanged}/> <br/>
-            <input type="text" placeholder="Shim Thickness"  name={id} onChange={this.props.thicknessChanged}/>
+            <input type="text" placeholder="Shim Thickness"  name={id} onChange={this.props.thicknessChanged}/> <br/><br/>
+            
+            <label>New Shim Thickness</label> <br/>
+            <input type="text" disabled value={newThickness}/> <br/>  
          </div>
       )
    }
